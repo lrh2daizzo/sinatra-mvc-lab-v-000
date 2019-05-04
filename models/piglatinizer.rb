9a6411517bb7c33ae72cc5cc5d7bed25994b47c9
Luis Herrera <herrera.lrh@gmail.com>
2019-05-04 01:24:18 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,10 +2,7 @@ require 'pry'
 
 class PigLatinizer
 
-  def piglatinize(input_word)
-    input_word.split(" ").length == 1 ? piglatinize_word(input_word) : piglatinize_sentence(input_word)
-  end
-
+  
   def consonant?(char)
     !char.match(/[aAeEiIoOuU])
   end
