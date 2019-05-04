167be2294dd3d9619e1a53d40ccfbf3759fc012b
Luis Herrera <herrera.lrh@gmail.com>
2019-05-04 01:09:45 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,9 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(input_word)
-    input_word binding.pry
+    input_word.split(" ").length == 1 ? piglatinize_word(input_word) : piglatinize_sentence(input_word)
   end
+
+  
 
 end
