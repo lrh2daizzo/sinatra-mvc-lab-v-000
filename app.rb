0176d6ae7aa8f77013971e4b053ea00944bf0f75
Luis Herrera <herrera.lrh@gmail.com>
2019-05-04 00:32:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require_relative 'config/environment'
 require_relative 'models/piglatinizer.rb'
+
 class App < Sinatra::Base
   get '/' do
+    erb :user_input
+  end
+
+  post '/' do
     erb :user_input
   end
 end
